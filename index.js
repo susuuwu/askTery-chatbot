89c4b6e406d9095d17aa2464e2bc389e4c83b196
SuUwU <90884137+susuuwu@users.noreply.github.com>
2022-03-08 00:12:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {WebhookClient} = require('dialogflow-fulfillment');
+const sheetdb = require("sheetdb-node");
+const client = sheetdb({ address: '8jb1q0tui9ikn' });
 
 const app = express()
 app.use(bodyParser.json())
@@ -20,9 +22,17 @@ const dialogflowFulfillment = (request, response) => {
     function sayHello(agent){
         agent.add("hi, this response is coming from heroku")
     }
+    
+    function saveToDB(agent) {
+        const rate = request.body.queryResult.parameter.value1;
+        client.create({ name: "William", age: 25 }).then(function(data) {
+    console.log(data);}, function(err){console.log(err);});
+        agent.add("Finish storing rating.")
+    }
 
     let intentMap = new Map();
     intentMap.set("testintent", sayHello)
+    intentMap.set("end.conversation - rating", saveToDB)
     agent.handleRequest(intentMap)
 
 }
